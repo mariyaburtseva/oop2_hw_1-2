778c59e9c7d0396b7a65c2f8f8f699d8e7928576
Burtseva Mariya <mariyaburceva9898@mail.ru>
2021-03-19 00:41:55 +0300

    Added package manager and class PostManager

--- src/main/java/manager/PostManager.java
+++ src/main/java/manager/PostManager.java
@@ -1,2 +1,19 @@
-package manager;public class PostManager {
+package manager;
+
+import domain.ManagerPost;
+
+public class PostManager {
+    private ManagerPost[] search;
+
+    public ManagerPost[] search() {
+        // TODO: add logic
+        return null;
+    }
+
+    public void delete() {
+        // TODO: add logic
+    }
 }
+
+
+
