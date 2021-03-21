1d02bb62965d8ea7d7799f233403b5e8bb88c94c
Burtseva Mariya <mariyaburceva9898@mail.ru>
2021-03-21 21:17:01 +0300

    added parametes to PostManager

--- src/main/java/domain/ManagerPost.java
+++ src/main/java/domain/ManagerPost.java
@@ -1,10 +1,10 @@
 package domain;
 
 public class ManagerPost {
-    private int owner_id;//идентификатор пользователя или сообщества.
+    private int ownerId;//идентификатор пользователя или сообщества.
     private String domain;//короткий адрес пользователя или сообщества
     private String query;//поисковой запрос. Для точного результата запрос необходимо передавать в двойных кавычках.
-    private boolean owners_only;//1 — возвращать только записи от имени владельца стены.Флаг, может принимать значения 1 или 0
+    private boolean ownersOnly;//1 — возвращать только записи от имени владельца стены.Флаг, может принимать значения 1 или 0
     private int count = 20;//количество записей, которые необходимо вернуть.положительное число, по умолчанию 20, максимальное значение 100
     private int offset;//смещение, необходимо для получения определенного подмножества результатов.положительное число, по умолчанию 0
     private boolean extended;//1 — в ответе будут возвращены дополнительные поля profiles и groups, содержащие информацию о пользователях и сообществах. По умолчанию: 0.флаг, может принимать значения 1 или 0
@@ -39,12 +39,12 @@ public class ManagerPost {
         //TODO: add logic
         return null;
     }
-    public int getOwner_id() {
-        return owner_id;
+    public int getOwnerId() {
+        return ownerId;
     }
 
-    public void setOwner_id(int owner_id) {
-        this.owner_id = owner_id;
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
     }
 
     public String getDomain() {
@@ -63,12 +63,12 @@ public class ManagerPost {
         this.query = query;
     }
 
-    public boolean isOwners_only() {
-        return owners_only;
+    public boolean isOwnersOnly() {
+        return ownersOnly;
     }
 
-    public void setOwners_only(boolean owners_only) {
-        this.owners_only = owners_only;
+    public void setOwnersOnly(boolean ownersOnly) {
+        this.ownersOnly = ownersOnly;
     }
 
     public int getCount() {
--- src/main/java/manager/PostManager.java
+++ src/main/java/manager/PostManager.java
@@ -1,19 +1,17 @@
 package manager;
-
 import domain.ManagerPost;
 
 public class PostManager {
     private ManagerPost[] search;
 
-    public ManagerPost[] search() {
+    public ManagerPost[] search(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset, boolean extended) {
         // TODO: add logic
         return null;
     }
 
-    public void delete() {
+    public void delete(int ownerId, int postId) {
         // TODO: add logic
     }
 }
-
 
 
